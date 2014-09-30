a5554eac405f4e8f68118ec5da52f43fe68db1c5
igalna <igalna@gmail.com>
2014-09-30 21:07:27 +0100

    added "askName()" method to ask for a users name "main()" method prints the users name

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,15 @@
+import java.util.Scanner;
+
 public class HelloWorld {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Your name is " + askName());
     }
+    public static String askName() {
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Enter your name > ");
+        return keyboard.nextLine();
+    }
+    
 }
